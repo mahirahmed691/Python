853cff33d42a5ed52f78acc14772b13fc27b32c1
mahirahmed691 <mahirahmed691@gmail.com>
2021-04-10 02:56:12 +0100

    Python Basics Refresh

---
+++ PythonBasics/002.py
@@ -0,0 +1,7 @@
+#! Defining Variables
+
+name = 'Mahir'
+surname = 'Ahmed'
+age = 27
+
+print('My name is ' + name + ' ' + surname + ' and I am ' + str(age) + ' years old.')
---
+++ PythonBasics/003.py
@@ -0,0 +1,6 @@
+#! Python Tuples
+
+tup1 = ('Robert', 'Carlos','1965','Terminator 1995', 'Actor','Florida');
+tup2 = (1, 2, 3, 4, 5, 6, 7)
+print(tup1[3])
+print(tup2[1:4])
