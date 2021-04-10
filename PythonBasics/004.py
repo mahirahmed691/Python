364851259f17ef6375ca41306d054ef6a0a4a8a3
mahirahmed691 <mahirahmed691@gmail.com>
2021-04-10 03:29:57 +0100

    Python dictionaries append

---
+++ PythonBasics/004.py
@@ -0,0 +1,10 @@
+#! Python dictionary append
+
+print ('#! Python dictionary append');
+
+my_dict = {"Name": [], "Proffesion": [], "Age": []}
+
+my_dict["Name"].append("Mahir Ahmed")
+my_dict["Proffesion"].append("Developer")
+my_dict["Age"].append(27)
+print(my_dict)
