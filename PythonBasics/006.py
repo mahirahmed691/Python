98a6ba2cc24671f25f86800a7afadb015aa9a037
mahirahmed691 <mahirahmed691@gmail.com>
2021-04-10 15:35:57 +0100

    Python string methods

---
+++ PythonBasics/006.py
@@ -0,0 +1,14 @@
+#! Python string methods
+
+print('#! Python string methodss')
+name = 'Mahir Ahmed'
+
+
+print(name.replace('Ahmed', '...'))
+print(name.capitalize())
+print(name.lower())
+print(name.upper())
+print(':'.join('python'))
+print(''.join(reversed(name)))
+print(len(name))
+
