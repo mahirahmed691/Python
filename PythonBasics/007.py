d7121347dc043f5488efa2aa1387b82738dfa435
mahirahmed691 <mahirahmed691@gmail.com>
2021-04-10 15:51:52 +0100

    Python defining functions

---
+++ PythonBasics/007.py
@@ -0,0 +1,14 @@
+#! Python functions
+
+def func1():
+    print('I am learning Python Functions')
+
+def square(x):
+    print(x * x)
+    
+def add(x, y):
+    return x + y
+
+func1()
+square(4)
+print(add(1, 2))
