2157256af95d3248f512d67035764ac87a80c687
mahirahmed691 <mahirahmed691@gmail.com>
2021-04-10 03:42:53 +0100

    The main method

---
+++ PythonBasics/005.py
@@ -0,0 +1,12 @@
+#! Python main
+
+
+def main():
+    print('This is being printed on the main method')
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
+
+print("Introduction to the python main method")
+print("Value in built variable name is:  ", __name__)
