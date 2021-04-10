f1a2e097290b8245b3708ef6d7bdacb118b81e6c
mahirahmed691 <mahirahmed691@gmail.com>
2021-04-10 03:35:54 +0100

    Python dict add and delete

--- PythonBasics/004.py
+++ PythonBasics/004.py
@@ -8,3 +8,10 @@ my_dict["Name"].append("Mahir Ahmed")
 my_dict["Proffesion"].append("Developer")
 my_dict["Age"].append(27)
 print(my_dict)
+
+
+## Delete python dicts
+print('\n')
+print ('## Delete python dicts with keys')
+del my_dict['Proffesion']  # This will remove the element with your key.
+print(my_dict)
